0a5713faf9ab9cdfa8ff1c59ed8bb7d136814ed2
Johannes Alneberg <alneberg@kth.se>
2023-02-09 15:11:49 +0100

    Got rid of the config file

--- daily_read/config.py
+++ daily_read/config.py
@@ -5,29 +5,15 @@ from dotenv import load_dotenv
 
 
 class Config(object):
-    def __init__(self, config_file):
+    def __init__(self):
 
         load_dotenv()
 
-        conf = {}
-        try:
-            with open(config_file, "r") as f:
-                content = yaml.load(f, Loader=yaml.SafeLoader)
-                conf.update(content)
-        except IOError as e:
-            pass
-
-        self.ORDER_PORTAL_URL = os.getenv("DAILY_READ_ORDER_PORTAL_URL", conf.get("DAILY_READ_ORDER_PORTAL_URL"))
-        self.ORDER_PORTAL_API_KEY = os.getenv(
-            "DAILY_READ_ORDER_PORTAL_API_KEY", conf.get("DAILY_READ_ORDER_PORTAL_API_KEY")
-        )
-        self.REPORTS_LOCATION = os.getenv("DAILY_READ_REPORTS_LOCATION", conf.get("DAILY_READ_REPORTS_LOCATION"))
-        self.DATA_LOCATION = os.getenv("DAILY_READ_DATA_LOCATION", conf.get("DAILY_READ_REPORTS_LOCATION"))
-        self.STHLM_STATUSDB_URL = os.getenv("DAILY_READ_STHLM_STATUSDB_URL", conf.get("DAILY_READ_STHLM_STATUSDB_URL"))
-        self.STHLM_STATUSDB_USERNAME = os.getenv(
-            "DAILY_READ_STHLM_STATUSDB_USERNAME", conf.get("DAILY_READ_STHLM_STATUSDB_USERNAME")
-        )
-        self.STHLM_STATUSDB_PASSWORD = os.getenv(
-            "DAILY_READ_STHLM_STATUSDB_PASSWORD", conf.get("DAILY_READ_STHLM_STATUSDB_PASSWORD")
-        )
+        self.ORDER_PORTAL_URL = os.getenv("DAILY_READ_ORDER_PORTAL_URL")
+        self.ORDER_PORTAL_API_KEY = os.getenv("DAILY_READ_ORDER_PORTAL_API_KEY")
+        self.REPORTS_LOCATION = os.getenv("DAILY_READ_REPORTS_LOCATION")
+        self.DATA_LOCATION = os.getenv("DAILY_READ_DATA_LOCATION")
+        self.STHLM_STATUSDB_URL = os.getenv("DAILY_READ_STHLM_STATUSDB_URL")
+        self.STHLM_STATUSDB_USERNAME = os.getenv("DAILY_READ_STHLM_STATUSDB_USERNAME")
+        self.STHLM_STATUSDB_PASSWORD = os.getenv("DAILY_READ_STHLM_STATUSDB_PASSWORD")
         self.SNPSEQ_URL = os.getenv("DAILY_READ_SNPSEQ_URL")
