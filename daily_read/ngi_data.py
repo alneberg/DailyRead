0b1fe1fb46631c37efea7eb0220534077f58b7b3
Johannes Alneberg <alneberg@kth.se>
2023-03-01 15:31:04 +0100

    Starting with git python

--- daily_read/ngi_data.py
+++ daily_read/ngi_data.py
@@ -1,3 +1,5 @@
+import git
+import gitdb
 import json
 import logging
 import os
@@ -10,9 +12,46 @@ class ProjectDataMaster(object):
         self.config = config
         self.sources = sources
         self.source_names = [source.name for source in sources]
+
+        self.data_repo = self.__setup_data_repo()
+
         self.__data_fetched = False
 
+    def __setup_data_repo(self):
+        # Safety check of path
+        if not os.path.isabs(data_location):
+            raise ValueError(f"Data location is not an absolute path: {data_location}")
+
+        if os.path.exists(data_location) and not os.path.isdir(data_location):
+            raise ValueError(f"Data Location exists but is not a directory: {data_location}")
+
+        # This seems to work with both existing git repos, empty directories
+        # and non-existing directories
+        data_repo = git.Repo.init(data_location)
+
+        # Make sure there is at least 1 commit (ref HEAD exists)
+        try:
+            self.data_repo.index.diff("HEAD")
+        except gitdb.exc.BadName as e:
+            # No commit exists yet
+            # Make sure no files are staged to be commited
+            if any([self.data_repo.is_dirty(), self.data_repo.untracked_files]):
+                raise ValueError(
+                    f"Data location has no commits but has modifications, "
+                    "please commit those or use an empty directory as the data location"
+                )
+
+            # Create empty file to include in the commit
+            new_file_path = os.path.join(data_location, ".empty")
+            open(new_file_path, "a").close()
+            self.data_repo.index.add([new_file_path])
+            self.index.commit("Added an empty file as a first commit")
+
+        return data_repo
+
     def get_data(self):
+        """Downloads data for each source into memory"""
+
         for source in self.sources:
             try:
                 source.get_data()
@@ -26,8 +65,12 @@ class ProjectDataMaster(object):
     def save_data(self):
         assert self.__data_fetched
         data_location = self.config.DATA_LOCATION
-        if not os.path.isdir(data_location):
-            raise ValueError(f"Data Location is not a directory: {data_location}")
+
+        if self.data_repo.is_dirty() or self.data_repo.untracked_files:
+            if self.any_modified_projects():
+                log.info("Changes for projects detected prior to fetching data!")
+                for project_id, ngi_node in self.get_modified_or_new_projects():
+                    log.info(f"{project_id} from {ngi_node} had changes not yet reported.")
 
         for source in self.sources:
             source_dir = os.path.join(data_location, source.dirname)
@@ -44,9 +87,29 @@ class ProjectDataMaster(object):
                     log.debug(f"Writing data for {project_id} to {file_name}")
                     fh.write(json.dumps(project_data))
 
-    def get_modified_projects(self):
-        # TODO Here go git stuff
-        return ["orderportal_id2", "orderportal_id3"]
+    def any_modified_or_new(self):
+        """Checks if there are modified or new projects and returns True or False.
+
+        True if any of these files are found:
+         - Modified and staged
+         - Modified but not staged
+         - Untracked files
+
+        """
+
+        return any([self.data_repo.is_dirty(), self.data_repo.untracked_files, self.data_repo.diff("HEAD")])
+
+    def get_modified_or_new_projects(self):
+        """Need to return files which are either:
+        - Modified and staged
+        - Modified but not staged
+        - Untracked files
+        """
+
+        if self.data_repo.is_dirty() or self.data_repo.untracked_files:
+            pass  # Might be tricky to find added files? At least when there's no commit already?
+
+        return [("orderportal_id2", "STHLM"), ("orderportal_id3", "STHLM")]
 
     def stage_data_for_project(self, project_id):
         pass
